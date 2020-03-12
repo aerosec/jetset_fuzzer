90488044b2166dcf0c83913a7f65728d368387ee
Maxwell Bland <mbland@ucsd.edu>
2020-03-12 13:00:46 -0400

    Use bindmounts to prevent disk space overflow and delays due to file syncronization in volume

--- scripts/afl-fuzz-ex.sh
+++ scripts/afl-fuzz-ex.sh
@@ -2,9 +2,9 @@
 mkdir outputs
 for i in {0..24}; do 
     if [ $i = 0 ] && [ "$1" = "f" ]; then
-	      sudo docker run --cap-add ALL --privileged --tmpfs /run -v ~/Projects/aerosec/cmu.fuzz/afl/syncdir:/usr/src/app/afl/syncdir bland_fuzz ./runall.sh -n "$1${i}" -p $i > ./outputs/$i 2>&1 &
+	      sudo docker run --cap-add ALL --privileged --tmpfs /run --mount type=bind,source=/home/mbland/Projects/aerosec/cmu.fuzz/afl/syncdir,target=/usr/src/app/afl/syncdir bland_fuzz ./runall.sh -n "$1${i}" -p $i > ./outputs/$i 2>&1 &
     else
         # all other fuzzers are secondary
-	      sudo docker run --cap-add ALL --privileged --tmpfs /run -v ~/Projects/aerosec/cmu.fuzz/afl/syncdir:/usr/src/app/afl/syncdir bland_fuzz ./runall.sh -t S -n "$1${i}" -p $i > ./outputs/$i 2>&1 &
+	      sudo docker run --cap-add ALL --privileged --tmpfs /run --mount type=bind,source=/home/mbland/Projects/aerosec/cmu.fuzz/afl/syncdir,target=/usr/src/app/afl/syncdir bland_fuzz ./runall.sh -t S -n "$1${i}" -p $i > ./outputs/$i 2>&1 &
     fi
 done
