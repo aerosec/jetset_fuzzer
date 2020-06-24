4fb92b010fbe2b190d2ea715f56ef7e127b5f947
Maxwell Bland <mbland@ucsd.edu>
2020-06-24 14:50:18 -0400

    Adjust pwd command in fuzzer scripts and make name of afl-fuzz-ex more descriptive.

--- scripts/afl-fuzz-ex.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-for i in {0..27}; do 
-    if [ $i = 0 ]; then
-	      sudo docker run --cap-add ALL --privileged --tmpfs /run -v $(PWD)/afl/syncdir:/usr/src/app/afl/syncdir -v /mnt/tmpfs:/mnt/tmpfs bland_fuzz ./runall.sh -n "$1${i}" -p $i -f /mnt/tmpfs/ > /dev/null 2>&1 &
-    else
-        # all other fuzzers are secondary
-	      sudo docker run --cap-add ALL --privileged --tmpfs /run -v $(PWD)/afl/syncdir:/usr/src/app/afl/syncdir -v /mnt/tmpfs:/mnt/tmpfs bland_fuzz ./runall.sh -t S -n "$1${i}" -p $i -f /mnt/tmpfs/ > /dev/null 2>&1 &
-    fi
-done
